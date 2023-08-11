3405c7b7405e8e48cac3a2a4e664cc9b5100c3ac
thetazero <lesha.seletskiy@gmail.com>
2023-08-10 21:52:02 -0700

    render recipe markdown

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+use comrak::{markdown_to_html, ComrakOptions};
 fn main() {
     println!("Hello, world!");
+
+    compile_recipes("./recipes");
+}
+
+fn compile_recipes(path: &str) {
+    let paths = fs::read_dir(path).unwrap();
+    for path in paths {
+        let content = fs::read_to_string(path.unwrap().path()).unwrap();
+        let markdown = markdown_to_html(&content, &ComrakOptions::default());
+        println!("{markdown}")
+    }
 }
