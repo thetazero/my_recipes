92e3e1343157c2c06c65e34c33ffcaa1990403dd
thetazero <lesha.seletskiy@gmail.com>
2023-08-12 23:03:57 -0700

    better titles and dark theme

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,8 @@ struct RecipeTemplate<'a> {
     markdown: &'a str,
 }
 fn main() {
-    println!("Hello, world!");
-
     compile_recipes("./recipes");
+    copy_assets();
 }
 
 fn compile_recipes(path: &str) {
@@ -22,11 +21,45 @@ fn compile_recipes(path: &str) {
         let markdown = markdown_to_html(&content, &ComrakOptions::default());
 
         let recipe_name = path.file_name().unwrap().to_str().unwrap();
+        let nice_name = procees_title_string(recipe_name);
 
         let rendered_page = RecipeTemplate {
-            name: recipe_name,
+            name: &nice_name,
             markdown: &markdown,
-        }.to_string();
-        fs::write("./built/".to_owned() + recipe_name + ".html", &rendered_page).unwrap();
+        }
+        .to_string();
+        fs::write(
+            "./built/".to_owned() + recipe_name + ".html",
+            &rendered_page,
+        )
+        .unwrap();
     }
+}
+
+fn copy_assets() {
+    let paths = fs::read_dir("./assets").unwrap();
+    for path in paths {
+        let path = path.unwrap().path();
+        let file_name = path.file_name().unwrap().to_str().unwrap();
+        fs::copy(&path, "./built/".to_owned() + file_name).expect("Failed to copy asset");
+    }
+}
+
+fn procees_title_string(title: &str) -> String {
+    let mut result = String::new();
+    result.push_str(&title[0..1].to_uppercase());
+    for c in title[1..].chars(){
+        match c {
+            '_' => {
+                result.push(' ');
+            }
+            '.' => {
+                break;
+            }
+            _ => {
+                result.push(c);
+            }
+        }
+    }
+    result
 }
