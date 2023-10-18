fc4615bfa2c2466b49a3cf5bccb55ebf53f1194e
thetazero <lesha.seletskiy@gmail.com>
2023-10-18 17:09:27 +0000

    Deploying to gh-pages from @ thetazero/my_recipes@9d9e6959fc5e9a12c69cbabeb0a29e17836f5d20 🚀

---
+++ index.js
@@ -0,0 +1,82 @@
+export default [
+    {
+        "name": "Briam",
+        "path": "briam.md.html"
+    },
+    {
+        "name": "Bulgur salad",
+        "path": "bulgur_salad.md.html"
+    },
+    {
+        "name": "Caper vinegarette",
+        "path": "caper_vinegarette.md.html"
+    },
+    {
+        "name": "Chicken chow mein",
+        "path": "chicken_chow_mein.md.html"
+    },
+    {
+        "name": "Chicken mushroom stirfry",
+        "path": "chicken_mushroom_stirfry.md.html"
+    },
+    {
+        "name": "Chicken schnitzel",
+        "path": "chicken_schnitzel.md.html"
+    },
+    {
+        "name": "Chicken tangine",
+        "path": "chicken_tangine.md.html"
+    },
+    {
+        "name": "Eggplant parm",
+        "path": "eggplant_parm.md.html"
+    },
+    {
+        "name": "Fall salad",
+        "path": "fall_salad.md.html"
+    },
+    {
+        "name": "Garlic vinegar dipping sauce",
+        "path": "garlic_vinegar_dipping_sauce.md.html"
+    },
+    {
+        "name": "Hummus",
+        "path": "hummus.md.html"
+    },
+    {
+        "name": "Israeli couscous",
+        "path": "israeli_couscous.md.html"
+    },
+    {
+        "name": "Khinkali",
+        "path": "khinkali.md.html"
+    },
+    {
+        "name": "Meatballs",
+        "path": "meatballs.md.html"
+    },
+    {
+        "name": "Moussaka",
+        "path": "moussaka.md.html"
+    },
+    {
+        "name": "Pad see ew",
+        "path": "pad_see_ew.md.html"
+    },
+    {
+        "name": "Pepper casserole",
+        "path": "pepper_casserole.md.html"
+    },
+    {
+        "name": "Plov",
+        "path": "plov.md.html"
+    },
+    {
+        "name": "Spinach and cheese bourekas",
+        "path": "spinach_and_cheese_bourekas.md.html"
+    },
+    {
+        "name": "Spring salad",
+        "path": "spring_salad.md.html"
+    }
+]
